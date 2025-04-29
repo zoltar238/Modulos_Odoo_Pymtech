9a5918e49d7014fb7af9a989f404cc8c932dd65b
Andrei <protoandrei@protonmail.com>
2025-04-29 10:49:36 +0200

    update backup script to use windows batch commands instead of bash

--- autobackup/backup_odoo_windows.bat
+++ autobackup/backup_odoo_windows.bat
@@ -2,20 +2,26 @@
 setlocal EnableDelayedExpansion
 
 REM vars
-SET "BACKUP_DIR=~\odoo_backups"
+SET "BACKUP_DIR=%USERPROFILE%\odoo_backups"
 SET "ODOO_URL=http://nibafa.es"
 SET "ODOO_DATABASE=sandbox"
 SET "ADMIN_PASSWORD=hpyc-evt9-5pwt"
 SET "KEPT_BACKUPS=7"
 
-REM create a backup directory
-mkdir "-p" "%BACKUP_DIR%"
+REM create a backup directory if it doesn't exist
+if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%"
+
+REM Get current date in YYYY-MM-DD format using PowerShell
+for /f %%i in ('powershell -Command "Get-Date -Format 'yyyy-MM-dd'"') do set TODAY_DATE=%%i
 
 REM create a backup
-SET _INTERPOLATION_0=
-FOR /f "delims=" %%a in ('date +%F') DO (SET "_INTERPOLATION_0=!_INTERPOLATION_0! %%a")
-curl "-X" "POST" "-F" "master_pwd=%ADMIN_PASSWORD%" "-F" "name=%ODOO_DATABASE%" "-F" "backup_format=zip" "-o" "!BACKUP_DIR!/!ODOO_DATABASE!.!_INTERPOLATION_0:~1!.zip" "!ODOO_URL!\web\database\backup"
-
+echo Creating backup file: %BACKUP_DIR%\%ODOO_DATABASE%.%TODAY_DATE%.zip
+curl -X POST -F "master_pwd=%ADMIN_PASSWORD%" -F "name=%ODOO_DATABASE%" -F "backup_format=zip" -o "%BACKUP_DIR%\%ODOO_DATABASE%.%TODAY_DATE%.zip" "%ODOO_URL%/web/database/backup"
+echo Backup command finished. Check the backup directory.
 
 REM delete old backups
-find "!BACKUP_DIR!" "-type" "f" "-mtime" "+!KEPT_BACKUPS!" "-name" "!ODOO_DATABASE!.*.zip" "-delete"
+echo Deleting backups older than %KEPT_BACKUPS% days...
+forfiles /P "%BACKUP_DIR%" /M "%ODOO_DATABASE%.*.zip" /D -%KEPT_BACKUPS% /C "cmd /c echo Deleting @file ... && del @path"
+echo Cleanup finished.
+
+endlocal
