6bdcf503fb2bf16eb94e36fe682e928fb0642bb6
Andrei <protoandrei@protonmail.com>
2025-05-12 11:39:46 +0200

    Initial Commit

--- scripts/log_manager/src/watchdog.py
+++
@@ -1,7 +0,0 @@
-import configparser
-
-# Read the configuration file
-config = configparser.ConfigParser()
-config.read("./res/watchdog.conf")
-email = config['email']['sender']
-key = config['email']['key']
